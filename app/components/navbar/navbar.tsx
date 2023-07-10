cc2897edea6b5530a60627bee8871a36842b96c4
techshomez <danieltopeg@gmail.com>
2023-07-10 12:35:18 +0100

    navbar component logo

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,10 +1,15 @@
 import Container from "../Container";
+import Logo from "./logo";
 
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
-        <Container></Container>
+        <Container>
+          <div className="flex flex-row item-center justify-between gap-3 md:gap-0">
+            <Logo />
+          </div>
+        </Container>
       </div>
     </div>
   );
