bf38fd7fa96dcfe3c4204b8723ad10d67526439a
techshomez <danieltopeg@gmail.com>
2023-08-04 13:24:31 +0100

    create mongodb database and creating the prisma schema

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -73,13 +73,15 @@ const RegisterModal = () => {
   );
 
   const footerContent = (
-    <div className="flex flex-col gap-4 mt-3">
+    <div className="flex flex-col gap-4">
       <hr />
       <Button
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => {
+          69;
+        }}
       />
       <Button
         outline
